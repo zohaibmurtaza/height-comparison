371f28acd50ee7217314c8baf2b917a0fe52de9d
Zohaib <zohaibwb38@gmail.com>
2025-02-04 12:31:14 +0500

    Sticky Footer height fixed

--- components/StickyFooter.tsx
+++ components/StickyFooter.tsx
@@ -3,7 +3,7 @@ import AdsenseUnit from "./google-ads/AdsenseUnit";
 
 const StickyFooter = () => {
   return (
-    <div className="w-full h-[100px] bg-white/80 border-t border-gray-100 z-[99999] fixed bottom-0 left-0 right-0">
+    <div className="w-full h-[100px] max-h-[100px] bg-white/80 border-t border-gray-100 z-[99999] fixed bottom-0 left-0 right-0">
       <AdsenseUnit slot="2400450591" format="auto" responsive={true} />
     </div>
   );
