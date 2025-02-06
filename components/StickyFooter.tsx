12150f5cb64d1afa8275c7783b46741eef38e5c3
hafizzohaibaslam <aamzzohaib@gmail.com>
2025-02-06 23:17:03 +0500

    Trying to fix footer height

--- components/StickyFooter.tsx
+++ components/StickyFooter.tsx
@@ -4,7 +4,10 @@ import AdsenseUnit from "@/components/google-ads/AdsenseUnit";
 const StickyFooter = () => {
   return (
     <div className="w-full !h-[100px] !max-h-[100px] bg-white/80 border-t border-gray-100 z-[99999] fixed bottom-0 left-0 right-0">
-      <div>
+      <div
+        className="!h-[100px] !max-h-[100px]"
+        style={{ height: "100px", maxHeight: "100px" }}
+      >
         <AdsenseUnit
           slot="2400450591"
           format="auto"
