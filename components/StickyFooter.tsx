914df7550c906f6dc9a4a83f47211598995c0ee0
hafizzohaibaslam <aamzzohaib@gmail.com>
2025-02-06 22:56:17 +0500

    Sticky Footer Height Added

--- components/StickyFooter.tsx
+++ components/StickyFooter.tsx
@@ -3,13 +3,18 @@ import AdsenseUnit from "@/components/google-ads/AdsenseUnit";
 
 const StickyFooter = () => {
   return (
-    <div className="w-full !h-[100px] !max-h-[100px] bg-white/80 border-t border-gray-100 z-[99999] fixed bottom-0 left-0 right-0">
-      <AdsenseUnit
-        slot="2400450591"
-        format="auto"
-        responsive={true}
-        className="!h-[100px] !max-h-[100px]"
-      />
+    <div
+      className="w-full !h-[100px] !max-h-[100px] bg-white/80 border-t border-gray-100 z-[99999] fixed bottom-0 left-0 right-0"
+      style={{ height: "100px", maxHeight: "100px" }}
+    >
+      <div>
+        <AdsenseUnit
+          slot="2400450591"
+          format="auto"
+          responsive={true}
+          className="!h-[100px] !max-h-[100px]"
+        />
+      </div>
     </div>
   );
 };
